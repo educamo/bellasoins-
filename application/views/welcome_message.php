d9a7cf50039b075290b700242fb7adb0104bffc4
C eduardo Carrasco <educamo@gmail.com>
2023-06-25 20:08:01 -0400

    se creo la seccion y se modifico la barra de menu

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -9,6 +9,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Home - Bella Soins Inc</title>
 
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,700;1,100;1,300;1,400&display=swap" rel="stylesheet">
+
 	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
@@ -33,7 +37,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 			</div>
 		</div>
 
-		<nav class="navbar sticky-top navbar-expand-lg bg-ligth">
+		<nav class="navbar mt-4 fixed-top navbar-expand-lg bg-light">
 			<div class="container-fluid text-center">
 				<a class="navbar-brand" href="#">Navbar</a>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -56,6 +60,27 @@ defined('BASEPATH') or exit('No direct script access allowed');
 		</nav>
 
 	</header>
+
+	<section id="slogan" class="container-fluid slogan pt-2">
+		<div class="row">
+			<div class="col-md-12 text-start">
+				<h1>Bella Soins Inc.</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<p>El lugar donde tu belleza se realza</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4 offset-md-2">
+				<a href="#" class="boton-citas">Agenda tu cita</a>
+			</div>
+			<div class="col-md-8 offset-md-4">
+				<img src="<?= base_url('assets/img/estetica.jpg') ?>" alt="Una mujer sonriente en una estética" class="img-thumbnail img-fluid rounded mx-auto d-block">
+			</div>
+		</div>
+	</section>
 
 
 
