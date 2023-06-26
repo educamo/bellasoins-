26ebdfb7bbfbfa63ae181bca30f67bba92a011f9
C eduardo Carrasco <educamo@gmail.com>
2023-06-26 02:06:29 -0400

    se creo la section servicios y se modifico la navbar

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -26,18 +26,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 <body>
 	<header>
-		<div class="container-fluid mb-2 text-info-cabecera">
-			<div class="row">
-				<div class="col-6 ps-3">
-					<a class="link-opacity-25 text-secondary link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="tel:+">00000000000</a>
-				</div>
-				<div class="col-6 text-end pe-5">
-					<a href="#" class="link-opacity-25 text-secondary link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">en</a>
-				</div>
-			</div>
-		</div>
 
-		<nav class="navbar mt-4 fixed-top navbar-expand-lg bg-light">
+		<nav class="navbar p-t-2 fixed-top navbar-expand-lg bg-light">
 			<div class="container-fluid text-center">
 				<a class="navbar-brand" href="#">Navbar</a>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -59,9 +49,20 @@ defined('BASEPATH') or exit('No direct script access allowed');
 			</div>
 		</nav>
 
+		<div class="container-fluid mt-5 text-info-cabecera" style="z-index: 500;">
+			<div class="row">
+				<div class="col-6 ps-3">
+					<a class="link-opacity-25 text-secondary link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="tel:+">00000000000</a>
+				</div>
+				<div class="col-6 text-end pe-5">
+					<a href="#" class="link-opacity-25 text-secondary link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">en</a>
+				</div>
+			</div>
+		</div>
+
 	</header>
 
-	<section id="slogan" class="container-fluid slogan pt-2">
+	<section id="slogan" class="container-fluid slogan">
 		<div class="row">
 			<div class="col-md-12 text-start">
 				<h1>Bella Soins Inc.</h1>
@@ -80,6 +81,98 @@ defined('BASEPATH') or exit('No direct script access allowed');
 				<img src="<?= base_url('assets/img/estetica.jpg') ?>" alt="Una mujer sonriente en una estética" class="img-thumbnail img-fluid rounded mx-auto d-block">
 			</div>
 		</div>
+	</section>
+
+	<section id="services" class="container mt-4 mb-4">
+		<div class="text-center">
+			<h1>Servicios</h1>
+		</div>
+
+		<div class="row">
+			<div class="col-md-4 mb-4">
+				<div class="card">
+					<img src="<?= base_url('assets/img/images.png') ?>" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4 mb-4">
+				<div class="card" aria-hidden="true">
+					<img src="..." class="card-img-top placeholder col-12 bg-dark" alt="..." id="placeholder-img">
+					<div class="card-body">
+						<h5 class="card-title placeholder-glow">
+							<span class="placeholder col-6"></span>
+						</h5>
+						<p class="card-text placeholder-glow">
+							<span class="placeholder col-7"></span>
+							<span class="placeholder col-4"></span>
+							<span class="placeholder col-4"></span>
+							<span class="placeholder col-6"></span>
+							<span class="placeholder col-8"></span>
+						</p>
+						<a class="btn btn-primary disabled placeholder col-6"></a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4 mb-4">
+				<div class="card">
+					<img class="card-img-top" src="<?= base_url('assets/img/images.png') ?>" alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title">Design</h5>
+						<p class="card-text">Web design, graphic design, logo design, branding, packaging design, UI/UX design</p>
+						<a href="#" class="btn btn-primary">Learn more</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4 mb-4">
+				<div class="card">
+					<img class="card-img-top" src="<?= base_url('assets/img/images.png') ?>" alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title">Development</h5>
+						<p class="card-text">Web development, mobile development, software development, e-commerce development</p>
+						<a href="#" class="btn btn-primary">Learn more</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4 mb-4">
+				<div class="card">
+					<img class="card-img-top" src="<?= base_url('assets/img/images.png') ?>" alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title">Marketing</h5>
+						<p class="card-text">Digital marketing, social media marketing, search engine optimization (SEO), content marketing</p>
+						<a href="#" class="btn btn-primary">Learn more</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4 mb-4">
+				<div class="card" aria-hidden="true">
+					<img src="..." class="card-img-top placeholder col-12 bg-dark" alt="..." id="placeholder-img">
+					<div class="card-body">
+						<h5 class="card-title placeholder-glow">
+							<span class="placeholder col-6"></span>
+						</h5>
+						<p class="card-text placeholder-glow">
+							<span class="placeholder col-7"></span>
+							<span class="placeholder col-4"></span>
+							<span class="placeholder col-4"></span>
+							<span class="placeholder col-6"></span>
+							<span class="placeholder col-8"></span>
+						</p>
+						<a class="btn btn-primary disabled placeholder col-6"></a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
 	</section>
 
 
